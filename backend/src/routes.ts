117ae387ec6b73a24710d594c0634734b3a6c506
Camargo1958 <aldrov_camargo@hotmail.com>
2022-08-24 19:27:02 -0300

    Criando middleware

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,10 @@
 import {Router, Request, Response} from 'express';
 
-import {CreateUserController} from './controllers/user/CreateUserController';
-
+import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
-import { hashSync } from 'bcryptjs';
+import { DetailUserController } from './controllers/user/DetailUserController';
+import { isAuthenticated } from './middlewares/isAuthenticated'
+
 
 const router = Router();
 
@@ -16,5 +17,7 @@ router.get('/teste', (req: Request, res: Response)=>{
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
+
+router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 export { router };
