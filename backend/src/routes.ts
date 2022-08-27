68427381cb590c39c60336e6f329316932726100
Camargo1958 <aldrov_camargo@hotmail.com>
2022-08-26 22:40:52 -0300

    Cadastrando Categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated'
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 
 const router = Router();
@@ -20,4 +21,7 @@ router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
-export { router };
+//-- ROTAS CATEGORY --
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+export { router }; 
