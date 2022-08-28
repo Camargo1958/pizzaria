db1c6da610f95f4e9b5a2c937584086167377067
Camargo1958 <aldrov_camargo@hotmail.com>
2022-08-28 10:16:05 -0300

    Listando Categorias

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated'
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
-
+import { ListCategoryController } from './controllers/category/ListCategoryController';
 
 const router = Router();
 
@@ -23,5 +23,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 //-- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router }; 
