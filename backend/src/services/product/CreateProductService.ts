365017856755914bf36bcf81fc61e1b913206d9f
Camargo1958 <aldrov_camargo@hotmail.com>
2022-09-18 23:46:57 -0300

    Criada rota de produtos

---
+++ backend/src/services/product/CreateProductService.ts
@@ -0,0 +1,18 @@
+import prismaClient from "../../prisma";
+
+interface ProductRequest{
+    name: String;
+    price: String;
+    description: String;
+    banner: String;
+    category_id: String;
+}
+
+class CreateProductService {
+    async execute({name,price,description,banner,category_id}:ProductRequest){
+
+        return {ok:true}
+    }
+}
+
+export {CreateProductService}
