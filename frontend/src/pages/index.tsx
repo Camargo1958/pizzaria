2ece646809ea049689953c2914f173195c187016
Camargo1958 <aldrov_camargo@hotmail.com>
2022-10-01 11:01:23 -0300

    Criando contexto

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import {useContext, FormEvent} from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import logoImg from '../../public/logo.svg'
@@ -5,8 +6,22 @@ import styles from '../../styles/home.module.scss'
 import {Input} from '../components/ui/Input'
 import {Button} from '../components/ui/Button'
 import Link from 'next/link'
+import {AuthContext} from '../contexts/AuthContext'
 
 export default function Home() {
+
+  const {signIn} = useContext(AuthContext)
+
+  let data = {
+    email: "algum@teste.com",
+    password: "123123"
+  }
+
+  async function handleLogin(event:FormEvent) {
+    event.preventDefault();
+    await signIn(data);
+  }
+
   return (
     <>
     <Head>
@@ -16,7 +31,7 @@ export default function Home() {
       <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
 
       <div className={styles.login}>
-        <form>
+        <form onSubmit={handleLogin}>
           <Input placeholder='Digite seu email' type="text"/>
           <Input placeholder='Sua senha' type="password"/>
           <Button type="submit" loading={false}>
